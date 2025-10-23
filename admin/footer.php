21fddefdc2b5f8c824082349e2a2d4ff125ad957
Phan Quốc Hưng <quochungdz2017@gmail.com>
2025-10-23 10:05:51 +0700

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,67 @@
+<footer>
+    <div class="footer-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-contact">
+                        <img src="#" alt="">
+                        <p>Trang web được xây dựng bởi Thành Đạt và Quốc Hưng, dưới đây là thông tin liên lạc: </p>
+                        <ul class="address">
+
+                            <li>
+                                <span class="fa fa-phone"></span>
+                                0334043054
+                            </li>
+                            <li>
+                                <span class="fa fa-envelope-o"></span>
+                                quochungdz2017@gmail.com
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 hidden-sm">
+                    <div class="footer-tweets">
+                        <div class="footer-title">
+                            <h3>Mô tả</h3>
+                        </div>
+                        <div class="twitter-text" style="float:left;">
+                            <p>Đây là trang web bán quần áo thời trang dựa trên trang web https://dirtycoins.vn/ được
+                                tạo ra nhằm phục vụ cho môn luận văn tốt nghiệp</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-support">
+                        <div class="footer-title">
+                            <h3>Họ và tên</h3>
+                        </div>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="#">Võ Thành Đạt</a></li>
+                                <li><a href="#">Phan Quốc Hưng</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-4">
+                    <div class="footer-info" style="width: 170px;">
+                        <div class="footer-title">
+                            <h3>Mã số sinh viên</h3>
+                        </div>
+                        <div class="footer-menu">
+                            <ul>
+
+                                <li><a href="#">2251120285</a></li>
+                                <li><a href="#">2251120292</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer top area end -->
+    <!-- footer area start -->
+
+    <a href="#" id="scrollUp"><i class="fa fa fa-arrow-up"></i></a>
+</footer>
